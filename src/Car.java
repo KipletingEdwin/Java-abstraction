c48969dade56c7a9d0e75d82c2ec8dad1959003f
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-02 12:45:55 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,7 @@
+public class Car extends  Vehicle{
+    @Override
+    void go(){
+        //TODO Auto-generated method stub
+        System.out.println("The driver is driving the car");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //abstract = abstract classes cannot be instantiated, but they can have a subclass
+        //           abstract methods are declared without an implementation
+        //Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+
+        car.go();
+
+    }
+}
